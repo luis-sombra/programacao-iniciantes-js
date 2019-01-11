4123579c8c098a2c3bbf635181145b7eac4b0f82
Unknown <contato@luissombra.com>
2019-01-11 14:55:11 -0300

    papel do javascript

---
+++ papel-javascript/app.js
@@ -0,0 +1,12 @@
+//insira seu código javscript aqui!
+function aplicarEstilo1(){
+    document.getElementById('titulo').className = "estilo1";
+}
+
+function aplicarEstilo2(){
+    document.getElementById('titulo').className = "estilo2";
+}
+
+function mudarConteudo(){
+    document.getElementById('paragrafo').innerHTML = "texto vindo do Javascript";
+}
