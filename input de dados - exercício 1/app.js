7ce1fafdf5801f81e838b7f42481959007447875
Unknown <contato@luissombra.com>
2019-01-13 05:19:26 -0300

    input de dados

---
+++ input de dados - exercício 1/app.js
@@ -0,0 +1,8 @@
+//insira seu código javscript aqui!
+var nome;
+var sobrenome;
+
+nome = prompt("Qual seu nome?");
+sobrenome = prompt("Qual seu sobrenome?");
+
+alert("Você se chama: " + nome + " " + sobrenome + "!");
