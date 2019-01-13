4e4aeb551425fcf128debbcbd9a7989d3e07cfb4
Unknown <contato@luissombra.com>
2019-01-13 05:20:03 -0300

    conversão entre strings e números

---
+++ conversao-strings-numeros/app.js
@@ -0,0 +1,8 @@
+//insira seu código javscript aqui!
+var result, x, y;
+x = prompt("Informe o primeiro número:");
+y = prompt("Informar o segundo número:");
+
+result = parseInt(x) + parseInt(y);
+
+alert(result);
