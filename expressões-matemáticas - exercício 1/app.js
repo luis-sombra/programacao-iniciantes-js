8472e6569e4221c0fa72bf15444386ad4904b861
Unknown <contato@luissombra.com>
2019-01-16 09:54:55 -0300

    expressões matemáticas - exercício 1

---
+++ expressões-matemáticas - exercício 1/app.js
@@ -0,0 +1,10 @@
+//insira seu código javscript aqui!
+var result, x, y, z;
+
+x = prompt("Informe o primeiro número:");
+y = prompt("Informe o segundo número:");
+z = prompt("informe o multiplicador:");
+
+result = (parseInt(x) + parseInt(y)) * parseInt(z);
+
+alert("O resultado é: " + result);
