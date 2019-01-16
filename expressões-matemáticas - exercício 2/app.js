24b73c65a87fd7285751edce64765c0025c13ae8
Unknown <contato@luissombra.com>
2019-01-16 09:55:13 -0300

    expressões matemáticas - exercício 2

---
+++ expressões-matemáticas - exercício 2/app.js
@@ -0,0 +1,9 @@
+//insira seu código javscript aqui!
+var idade, ano_corrente, ano_nascimento;
+ano_corrente = 2019;
+
+ano_nascimento = prompt("Informe o ano em que você nasceu:");
+
+idade = (ano_corrente - parseInt(ano_nascimento));
+
+alert("você tem " + idade + " anos");
